c925c83f7ad7999dc4157d0c3544a82ea7cac1c2
Jean-Luc GUILLAUME <jean-luc.guillaume@cpe.fr>
2021-03-06 20:54:26 +0100

    1

--- js/reservation.js
+++ js/reservation.js
@@ -2,8 +2,3 @@ var url=window.location.href;
 var indice =parseInt(url.substring( url.length - 1,url.length));
 
 Tab = [lyon, pyongyang, chernobyl,paris];
-
-function f() {
-    alert(String(Tab[indice].prix+0.99));
-  }
-;
