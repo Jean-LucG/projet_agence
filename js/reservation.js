4f5b872fdef1696b18df5bdc5c039a49d2473d50
Jean-Luc GUILLAUME <jean-luc.guillaume@cpe.fr>
2021-03-07 09:56:36 +0100

    1

--- js/reservation.js
+++ js/reservation.js
@@ -8,5 +8,5 @@ var nbre_enfants=1;
 var nbre_parents=3;
 var ptit_dej=1;
 //fin valeur test
-document.getElementById("prix").innerHTML="Prix Total: "+String(Tab[indice].prix*(nbre_parents+0.4*nbre_enfants)+ptit_dej*12);
+document.getElementById("prix").innerHTML="Prix Total: "+String(Tab[indice].prix*(nbre_parents+0.4*nbre_enfants)+ptit_dej*12)+"&euro;";
 
