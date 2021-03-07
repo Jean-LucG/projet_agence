403218f8f680ceb899b1056868152f2eb836373f
Jean-Luc GUILLAUME <jean-luc.guillaume@cpe.fr>
2021-03-07 09:43:28 +0100

    1

--- js/reservation.js
+++ js/reservation.js
@@ -4,13 +4,9 @@ var indice =parseInt(url.substring(url.length - 1,url.length));
 document.getElementById("voyage").innerHTML="Voyage à "+String(Tab[indice].lieu);
 
 //valeur test
-var nbre_enfants=2;
+var nbre_enfants=1;
 var nbre_parents=3;
 var ptit_dej=1;
+//fin valeur test
+document.getElementById("prix").innerHTML="Prix Total: "+String(Tab[indice].prix*(nbre_parents+0.4*nbre_enfants)+ptit_dej*12);
 
-
-
-function PrixTotal(){
-    document.getElementById("prix").innerHTML="Prix Total: "+String(Tab[indice].prix*(nbre_parents+0.4*nbre_enfants)+ptit_dej*12)
-};
-PrixTotal();
